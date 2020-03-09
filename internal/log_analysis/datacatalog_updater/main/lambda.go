e3ffaf6a05cd4384242a39bc763f8d8c74dfe582
Russell Leighton <russell.leighton@runpanther.io>
2020-03-09 11:01:41 -0400

    Add max retries to datacatalog_updater lambda (#372)
    * Put back last commit
    
    * Set MaxRetries to 20 for datacatalog_updater lambda

--- internal/log_analysis/datacatalog_updater/main/lambda.go
+++ internal/log_analysis/datacatalog_updater/main/lambda.go
@@ -24,6 +24,7 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-lambda-go/lambdacontext"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/glue"
 	"github.com/aws/aws-sdk-go/service/glue/glueiface"
@@ -38,9 +39,13 @@ import (
 	"github.com/panther-labs/panther/pkg/lambdalogger"
 )
 
+const (
+	maxRetries = 20 // setting Max Retries to a higher number - we'd like to retry VERY hard before failing.
+)
+
 var (
 	validation                   = validator.New()
-	glueClient glueiface.GlueAPI = glue.New(session.Must(session.NewSession()))
+	glueClient glueiface.GlueAPI = glue.New(session.Must(session.NewSession(aws.NewConfig().WithMaxRetries(maxRetries))))
 	// partitionPrefixCache is a cache that stores all the prefixes of the partitions we have created
 	// The cache is used to avoid attempts to create the same partitions in Glue table
 	partitionPrefixCache = make(map[string]struct{})

69f94a3ad3718e5eb6d2237777c2350f0493886e
Alexandros Sigalas <alxarch@gmail.com>
2020-05-26 19:51:29 +0300

    Types and helpers for `null` JSON value handling (#947)

---
+++ pkg/box/box.go
@@ -0,0 +1,64 @@
+// Package box provides boxing helpers for scalar values.
+// This package exists to help the transition form pointer based fields to `null` fields
+package box
+
+/**
+ * Panther is a Cloud-Native SIEM for the Modern Security Team.
+ * Copyright (C) 2020 Panther Labs Inc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// All helpers are inlined by the compiler
+
+func String(s string) *string {
+	return &s
+}
+func Int(n int) *int {
+	return &n
+}
+func Int8(n int8) *int8 {
+	return &n
+}
+func Int16(n int16) *int16 {
+	return &n
+}
+func Int32(n int32) *int32 {
+	return &n
+}
+func Int64(n int64) *int64 {
+	return &n
+}
+func Uint(n uint) *uint {
+	return &n
+}
+func Uint8(n uint8) *uint8 {
+	return &n
+}
+func Uint16(n uint16) *uint16 {
+	return &n
+}
+func Uint32(n uint32) *uint32 {
+	return &n
+}
+func Uint64(n uint64) *uint64 {
+	return &n
+}
+
+func Bool(b bool) *bool {
+	return &b
+}
+func Byte(b byte) *byte {
+	return &b
+}
---
+++ pkg/unbox/unbox.go
@@ -0,0 +1,105 @@
+// Package unbox provides unboxing helpers for scalar values
+package unbox
+
+/**
+ * Panther is a Cloud-Native SIEM for the Modern Security Team.
+ * Copyright (C) 2020 Panther Labs Inc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// All helpers are inlined and return the zero value if the pointer is nil
+
+func String(s *string) string {
+	if s != nil {
+		return *s
+	}
+	return ""
+}
+func Int(n *int) int {
+	if n != nil {
+		return *n
+	}
+	return 0
+}
+
+func Int8(n *int8) int8 {
+	if n != nil {
+		return *n
+	}
+	return 0
+}
+
+func Int16(n *int16) int16 {
+	if n != nil {
+		return *n
+	}
+	return 0
+}
+func Int32(n *int32) int32 {
+	if n != nil {
+		return *n
+	}
+	return 0
+}
+func Int64(n *int64) int64 {
+	if n != nil {
+		return *n
+	}
+	return 0
+}
+func Uint(n *uint) uint {
+	if n != nil {
+		return *n
+	}
+	return 0
+}
+func Uint8(n *uint8) uint8 {
+	if n != nil {
+		return *n
+	}
+	return 0
+}
+func Uint16(n *uint16) uint16 {
+	if n != nil {
+		return *n
+	}
+	return 0
+}
+func Uint32(n *uint32) uint32 {
+	if n != nil {
+		return *n
+	}
+	return 0
+}
+func Uint64(n *uint64) uint64 {
+	if n != nil {
+		return *n
+	}
+	return 0
+}
+
+func Bool(b *bool) bool {
+	if b != nil {
+		return *b
+	}
+	return false
+}
+
+func Byte(b *byte) byte {
+	if b != nil {
+		return *b
+	}
+	return 0
+}

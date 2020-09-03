955a31b1aa442487b87f4e8eeb9b3d657b859918
Russell Leighton <russell.leighton@runpanther.io>
2020-09-03 16:56:03 -0400

    Fix nil ptr in filterByTitleContains() (#1467)
    Co-authored-by: Nick Angelou <nick.angelou@runpanther.io>

--- internal/log_analysis/alerts_api/table/list.go
+++ internal/log_analysis/alerts_api/table/list.go
@@ -283,9 +283,9 @@ func filterByStatus(filter *expression.ConditionBuilder, input *models.ListAlert
 	}
 }
 
-// filterByTitleContains - fiters by a name that contains a string (case insensitive)
+// filterByTitleContains - filters by a name that contains a string (case insensitive)
 func filterByTitleContains(input *models.ListAlertsInput, alert *AlertItem) *AlertItem {
-	if alert != nil && input.NameContains != nil && !strings.Contains(
+	if alert != nil && input.NameContains != nil && alert.Title != nil && !strings.Contains(
 		strings.ToLower(*alert.Title),
 		strings.ToLower(*input.NameContains),
 	) {
@@ -295,7 +295,7 @@ func filterByTitleContains(input *models.ListAlertsInput, alert *AlertItem) *Ale
 	return alert
 }
 
-// filterByRuleIDContains - fiters by a name that contains a string (case insensitive)
+// filterByRuleIDContains - filters by a name that contains a string (case insensitive)
 func filterByRuleIDContains(input *models.ListAlertsInput, alert *AlertItem) *AlertItem {
 	if alert != nil && input.RuleIDContains != nil && !strings.Contains(
 		strings.ToLower(alert.RuleID),
@@ -307,7 +307,7 @@ func filterByRuleIDContains(input *models.ListAlertsInput, alert *AlertItem) *Al
 	return alert
 }
 
-// filterByAlertIDContains - fiters by a name that contains a string (case insensitive)
+// filterByAlertIDContains - filters by a name that contains a string (case insensitive)
 func filterByAlertIDContains(input *models.ListAlertsInput, alert *AlertItem) *AlertItem {
 	if alert != nil && input.AlertIDContains != nil && !strings.Contains(
 		strings.ToLower(alert.AlertID),
@@ -319,7 +319,7 @@ func filterByAlertIDContains(input *models.ListAlertsInput, alert *AlertItem) *A
 	return alert
 }
 
-// filterByEventCount - fiters by an eventCount defined by a range of two numbers
+// filterByEventCount - filters by an eventCount defined by a range of two numbers
 func filterByEventCount(filter *expression.ConditionBuilder, input *models.ListAlertsInput) {
 	// We are allowing either Min -or- Max to work together or independently
 	if input.EventCountMax != nil && input.EventCountMin != nil {

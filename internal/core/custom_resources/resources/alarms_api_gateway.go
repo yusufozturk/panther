55b316ba9a4bf6d63d2370fae7f107dd356769df
Kostas Papageorgiou <kostas@runpanther.io>
2020-09-18 10:27:16 +0300

    Changing evaluation period (#1551)

--- internal/core/custom_resources/resources/alarms_api_gateway.go
+++ internal/core/custom_resources/resources/alarms_api_gateway.go
@@ -85,13 +85,14 @@ func putGatewayAlarmGroup(props APIGatewayAlarmProperties) error {
 		return err
 	}
 
-	// 5XX errors are usually not problems in API gateway itself, but internal server error codes
-	// returned by the Lambda function which processed the request.
+	// 5XX errors can be triggered by GatewayAPI internal errors or Lambda execution errors.
+	// We don't have a way to differentiate between the two through Gateway API metrics. However, we have already
+	// alarms on AWS Lambda invocation failures that would capture errors caused by Lambda failures.
 	input.AlarmDescription = aws.String(fmt.Sprintf(
 		"API Gateway %s is reporting 5XX internal errors from its lambda handler. See: %s#%s",
 		props.APIName, alarmRunbook, props.APIName))
 	input.AlarmName = aws.String(fmt.Sprintf("Panther-%s-%s", gatewayErrorAlarm, props.APIName))
-	input.EvaluationPeriods = aws.Int64(1)
+	input.EvaluationPeriods = aws.Int64(3)
 	input.MetricName = aws.String("5XXError")
 	input.Period = aws.Int64(300)
 	input.Statistic = aws.String(cloudwatch.StatisticSum)

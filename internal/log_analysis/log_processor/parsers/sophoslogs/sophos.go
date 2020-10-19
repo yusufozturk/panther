dfdb294490c300d037a1b7b3c66516df0dbd1dfd
Ryan Moon <47795853+rynjms@users.noreply.github.com>
2020-10-19 02:03:18 -0700

    Parser for Sophos Central logs (#1770)
    * Parser for Sophos Central logs
    
    * Fix event time tag
    
    * Fixing failing test
    
    Co-authored-by: Kostas Papageorgiou <kostas@runpanther.io>
    Co-authored-by: Aggelos Arvanitakis <aggelos.arvanitakis@runpanther.io>
    Co-authored-by: Alexandros Sigalas <alxarch@gmail.com>

---
+++ internal/log_analysis/log_processor/parsers/sophoslogs/sophos.go
@@ -0,0 +1,33 @@
+package sophoslogs
+
+/**
+ * Panther is a Cloud-Native SIEM for the Modern Security Team.
+ * Copyright (C) 2020 Panther Labs Inc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import (
+	"github.com/panther-labs/panther/internal/log_analysis/log_processor/logtypes"
+)
+
+func init() {
+	logtypes.MustRegisterJSON(logtypes.Desc{
+		Name:         "Sophos.Central",
+		Description:  `Sophos Central events`,
+		ReferenceURL: `https://support.sophos.com/support/s/article/KB-000038307?language=en_US`,
+	}, func() interface{} {
+		return &SophosCentralEvent{}
+	})
+}
--- internal/log_analysis/log_processor/registry/registry.go
+++ internal/log_analysis/log_processor/registry/registry.go
@@ -38,6 +38,7 @@ import (
 	_ "github.com/panther-labs/panther/internal/log_analysis/log_processor/parsers/nginxlogs"
 	_ "github.com/panther-labs/panther/internal/log_analysis/log_processor/parsers/osquerylogs"
 	_ "github.com/panther-labs/panther/internal/log_analysis/log_processor/parsers/osseclogs"
+	_ "github.com/panther-labs/panther/internal/log_analysis/log_processor/parsers/sophoslogs"
 	_ "github.com/panther-labs/panther/internal/log_analysis/log_processor/parsers/suricatalogs"
 	_ "github.com/panther-labs/panther/internal/log_analysis/log_processor/parsers/sysloglogs"
 	_ "github.com/panther-labs/panther/internal/log_analysis/log_processor/parsers/zeeklogs"
